e19992c927575f51225be574790c2550edceb9c6
bong-pal <etgt777@naver.com>
2025-04-28 11:34:42 +0900

    169. Majority Element
    - 분할 정복 풀이

--- src/divideandconquer/Majority Element.kt
+++ src/divideandconquer/Majority Element.kt
@@ -8,18 +8,49 @@ fun main() {
     println(majorityElement(nums2))
 }
 
-// 보이어-무어 알고리즘
+// 분할 정복
 private fun majorityElement(nums: IntArray): Int {
-    var candidate = -1
-    var count = 0
+    return nums.findMajor(0, nums.lastIndex)
+}
 
-    for (n in nums) {
-        if (count == 0) candidate = n
+private fun IntArray.findMajor(left: Int, right: Int): Int {
+    if (left == right) return this[left]
 
-        if (candidate == n) count++ else count--
+    val mid = (left + right) / 2
+    val leftMajor = this.findMajor(left, mid)
+    val rightMajor = this.findMajor(mid + 1, right)
+
+    if (leftMajor == rightMajor) return leftMajor
+
+    val leftCount = this.countNum(left, mid, leftMajor)
+    val rightCount = this.countNum(mid + 1, right, rightMajor)
+
+    return if (leftCount > rightCount) leftMajor else rightMajor
+}
+
+private fun IntArray.countNum(start: Int, end: Int, num: Int): Int {
+    var result = 0
+
+    for (i in start..end) {
+        if (this[i] == num) result++
     }
 
-    return candidate
+    return result
 }
+
+
+//// 보이어-무어 알고리즘
+//private fun majorityElement(nums: IntArray): Int {
+//    var candidate = -1
+//    var count = 0
+//
+//    for (n in nums) {
+//        if (count == 0) candidate = n
+//
+//        if (candidate == n) count++ else count--
+//    }
+//
+//    return candidate
+//}
 
 // https://leetcode.com/problems/majority-element/description/
