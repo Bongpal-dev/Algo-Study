5d3a6e259cd9eb9fab23e2fe6e6a714170db032a
bong-pal <etgt777@naver.com>
2025-05-03 21:55:24 +0900

    2438번 별찍기 - 1

---
+++ src/baekjoon/2438.kt
@@ -0,0 +1,30 @@
+package baekjoon
+
+import java.io.ByteArrayInputStream
+
+fun main() {
+    val inputs = listOf(
+        "5"
+    )
+
+    println("------------------------------")
+    inputs.forEach {
+        System.setIn(ByteArrayInputStream(it.toByteArray()))
+        solve()
+        println("------------------------------")
+    }
+}
+
+private fun solve() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val sb = StringBuilder()
+
+    for (i in 1..n) {
+        repeat(i) { sb.append("*") }
+        if (i < n) sb.append("\n")
+    }
+
+    println(sb)
+}
+
+// https://www.acmicpc.net/problem/2438
