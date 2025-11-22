e7bbfe162b295adf836e3c55585bbeee013dbb6f
bong-pal <etgt777@naver.com>
2025-11-22 13:54:22 +0900

    3190. Find Minimum Operations to Make All Elements Divisible by Three

---
+++ src/leetcode/FindMinimumOperationsToMakeAllElementsDivisibleByThree.kt
@@ -0,0 +1,13 @@
+package leetcode
+
+fun main() {
+    val nums1 = intArrayOf(1, 2, 3, 4)
+    val nums2 = intArrayOf(3, 6, 9)
+
+    println(minimumOperations(nums1))
+    println(minimumOperations(nums2))
+}
+
+private fun minimumOperations(nums: IntArray) = nums.count { it % 3 != 0 }
+
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/
