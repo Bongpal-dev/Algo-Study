f7d010d41b11e9858fe3e9f07ee88fad76a6f956
bong-pal <etgt777@naver.com>
2025-12-03 22:40:55 +0900

    Q2. How Many Numbers Are Smaller Than the Current Number

---
+++ src/leetcode/quest/Array2/Q_2.kt
@@ -0,0 +1,15 @@
+package leetcode.quest.Array2
+
+fun main() {
+    val nums1 = intArrayOf(8, 1, 2, 2, 3)
+    val nums2 = intArrayOf(6, 5, 4, 8)
+    val nums3 = intArrayOf(7, 7, 7, 7)
+
+    println(smallerNumbersThanCurrent(nums1).joinToString())
+    println(smallerNumbersThanCurrent(nums2).joinToString())
+    println(smallerNumbersThanCurrent(nums3).joinToString())
+}
+
+private fun smallerNumbersThanCurrent(nums: IntArray) =  IntArray(nums.size) { i -> nums.count { nums[i] > it} }
+
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/
